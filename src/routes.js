eec4b4d0ca2a1a19439648f8e87bc8ddfa378b6a
falcon883 <durvankd@gmail.com>
2021-12-06 19:04:53 +0530

    Update routes.js

--- src/routes.js
+++ src/routes.js
@@ -10,25 +10,29 @@ const steam = new Steam()
 
 router.get('/user/:steamID64', async (req, res) => {
     let userSummary = await steam.getUserSummary(req.params.steamID64)
-
+    
+    res.status(userSummary.status_code ? userSummary.status_code : 200)
     res.json(userSummary);
 })
 
 router.get('/user/friends/:steamID64', async (req, res) => {
     let userFriends = await steam.getUserFriendList(req.params.steamID64)
-
+    
+    res.status(userFriends.status_code ? userFriends.status_code : 200)
     res.json(userFriends);
 })
 
 router.get('/user/games/:steamID64', async (req, res) => {
     let userGames = await steam.getUserOwnedGames(req.params.steamID64)
-
+    
+    res.status(userGames.status_code ? userGames.status_code : 200)
     res.json(userGames);
 })
 
 router.get('/user/gamesplayed/:steamID64', async (req, res) => {
     let userRecentPlayedGames = await steam.getUserRecentPlayedGames(req.params.steamID64)
-
+    
+    res.status(userRecentPlayedGames.status_code ? userRecentPlayedGames.status_code : 200)
     res.json(userRecentPlayedGames);
 })
 
@@ -38,6 +42,8 @@ router.get('/user/gamesplayed/:steamID64', async (req, res) => {
 
 router.get('/steamid/:steamid', async (req, res) => {
     let steamIDs = await Promise.all(req.params.steamid.split(',').map((id) => steam.convertSteamID(id)))
+    
+    res.status(steamIDs.status_code ? steamIDs.status_code : 200)
     res.json(steamIDs)
 })
 
